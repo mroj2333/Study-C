5ae4c2ae75614e7275d2f952ac121f2b634ff55c
Morax <mroj2333@sina.com>
2022-12-27 11:43:55 +0800

    指针强化

--- ChuanZhi_C/传智C提高/mycode/指针/02-野指针和空指针.c
+++ ChuanZhi_C/传智C提高/mycode/指针/02-野指针和空指针.c
@@ -1,3 +1,17 @@
 /*
-
+不允许向NULL或非法地址拷贝内存
 */
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  //空指针
+  char *p = NULL;
+  strcpy(p, "111"); // error 不允许给空指针赋值
+
+  // 野指针
+  char *p1 = 0x123346;
+  strcpy(p1, "1111"); // error 不允许给野指针赋值
+
+  return 0;
+}
